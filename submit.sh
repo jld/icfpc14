a65d47788d834a91031d27a287bb1d7462a88e7a
Jed Davis <jld@panix.com>
2014-07-25 23:05:44 -0700

    Cosmetic improvements to the bundling script.

--- submit.sh
+++ submit.sh
@@ -2,8 +2,10 @@
 set -e -u -x
 lman=$1
 tmpdir=$(mktemp -dt icfpc14.XXXXXXXXXX)
+final=${2:-submission.tgz}
 mkdir "$tmpdir/solution" "$tmpdir/code"
 cp "$lman" "$tmpdir/solution"
 git ls-tree -r --name-only HEAD | cpio -pdmv "$tmpdir/code"
-tar -zf submission.tgz -C "$tmpdir" -vc code solution
+tar -zf "$final" -C "$tmpdir" -vc code solution
 rm -r "$tmpdir"
+sha1sum "$final" || sha1 "$final" || openssl sha1 "$final"
